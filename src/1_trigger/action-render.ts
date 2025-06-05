ca4f18547d084c6045c47b5e0b42f41a01ce4f7e
Amy Manny <amyryanmanny@github.com>
2025-06-05 12:30:04 -0700

    Write md output to summary

--- src/1_trigger/action-render.ts
+++ src/1_trigger/action-render.ts
@@ -1,5 +1,5 @@
 import { renderTemplate } from "../4_template/render";
-import { getInput, setOutput } from "@actions/core";
+import { getInput, setOutput, summary } from "@actions/core";
 
 const template = getInput("template") || "main.md.vto";
 
@@ -7,3 +7,4 @@ const template = getInput("template") || "main.md.vto";
 const md = await renderTemplate(template);
 
 setOutput("md", md);
+summary.addRaw(md, true).write();
