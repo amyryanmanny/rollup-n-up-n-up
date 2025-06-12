1305da3946269b36fc07d23fb24d353141295169
Amy Manny <amyryanmanny@github.com>
2025-06-12 15:31:26 -0700

    Build

--- src/util/secrets/index.ts
+++ src/util/secrets/index.ts
@@ -4,6 +4,7 @@ import { getInput } from "@actions/core";
 
 export function getConfig(key: string): string | undefined {
   let config: string | undefined;
+
   // Flags set by CI/CD - https://stackoverflow.com/a/73973555
   if (process.env.GITHUB_ACTIONS === "true") {
     config = getInput(key);
