ee2f6c08995de3bf34a0898402721b024e0e8ea9
Amy Manny <amyryanmanny@github.com>
2025-07-10 12:59:31 -0700

    Add URL support which calls other client methods

--- src/2_pull/github/client.ts
+++ src/2_pull/github/client.ts
@@ -6,7 +6,62 @@ export class GitHubClient {
   // The Client class is a wrapper around the GitHub API client.
   public octokit = getOctokit();
 
-  // TODO: Support URL. Just decompose the URL and call the other methods
+  url(url: string): Promise<IssueList> {
+    const urlParts = new URL(url);
+    let match: RegExpMatchArray | null;
+
+    if (urlParts.hostname !== "github.com") {
+      // Troublemakers
+      throw new Error(
+        `Unsupported hostname: ${urlParts.hostname}. Please provide a valid GitHub URL.`,
+      );
+    }
+
+    // Repo Issues
+    match = urlParts.pathname.match(/\/([^/]+)\/([^/]+)\/issues/);
+    if (match) {
+      const owner = match[1];
+      const repo = match[2];
+
+      return this.issuesForRepo(owner, repo);
+    }
+
+    // Project View
+    match = urlParts.pathname.match(
+      /orgs\/([^/]+)\/projects\/(\d+)\/views\/(\d+)/,
+    );
+    if (match) {
+      const organization = match[1];
+      const projectNumber = parseInt(match[2], 10);
+      const projectViewNumber = parseInt(match[3], 10);
+
+      return this.issuesForProjectView(
+        organization,
+        projectNumber,
+        projectViewNumber,
+      );
+    }
+
+    // Project Query
+    match = urlParts.pathname.match(/orgs\/([^/]+)\/projects\/(\d+)/);
+    if (match) {
+      const organization = match[1];
+      const projectNumber = parseInt(match[2], 10);
+
+      // Extract the query from the URL search parameters
+      const customQuery = urlParts.searchParams.get("filterQuery") || "";
+
+      return this.issuesForProjectQuery(
+        organization,
+        projectNumber,
+        customQuery,
+      );
+    }
+
+    throw new Error(
+      `Unsupported URL: ${url}. Please provide a valid GitHub issues URL.`,
+    );
+  }
 
   issuesForRepo(owner: string, repo: string): Promise<IssueList> {
     return IssueList.forRepo({ owner, repo });
