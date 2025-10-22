3ba59289b2f4be882595198cda217b7b7cde4a27
Amy Manny <amyryanmanny@github.com>
2025-10-21 18:02:24 -0700

    Add SLACK_MUTE and duplicate message checking

--- src/5_push/slack/client.ts
+++ src/5_push/slack/client.ts
@@ -1,11 +1,28 @@
 import { getSlack } from "@util/slack";
+
 import { getChannelIdFromName } from "./channel";
 import { getDmIdFromGithubUsername } from "./dm";
+
+import { getConfig, isTruthy } from "@util/config";
+import { emitError, emitInfo } from "@util/log";
+
+import { isDuplicate } from "./util";
+
+export const SLACK_MUTE = isTruthy(getConfig("SLACK_MUTE"));
 
 export class SlackClient {
   public slack = getSlack();
 
   async sendToChannel(channelName: string, message: string) {
+    if (isDuplicate(channelName, message)) {
+      return;
+    }
+
+    if (SLACK_MUTE) {
+      emitInfo(`[SLACK_MUTE=true] Skipping Slack message to #${channelName}`);
+      return;
+    }
+
     const channelId = await getChannelIdFromName(channelName);
     const res = await this.send(channelId, message);
     if (!res.ok) {
@@ -16,10 +33,21 @@ export class SlackClient {
   }
 
   async sendDm(username: string, message: string) {
+    if (isDuplicate(username, message)) {
+      // Prevent sending duplicate messages in the same run
+      return;
+    }
+
+    if (SLACK_MUTE) {
+      emitInfo(`[SLACK_MUTE=true] Skipping Slack message to @${username}`);
+      return;
+    }
+
     const dmId = await getDmIdFromGithubUsername(username);
     const res = await this.send(dmId, message);
     if (!res.ok) {
-      throw new Error(`Failed to send Slack DM to ${username}: ${res.error}`);
+      // Don't throw, since we don't want to fail in the middle of sending DMs, it's hard to retry
+      emitError(`Failed to send Slack DM to ${username}: ${res.error}`);
     }
   }
 
--- src/5_push/slack/util.ts
+++ src/5_push/slack/util.ts
@@ -1,3 +1,13 @@
+const sentMessages = Array<{ target: string; message: string }>();
+export function isDuplicate(target: string, message: string): boolean {
+  // Prevent sending duplicate messages in the same run
+  const result = sentMessages.some(
+    (entry) => entry.target === target && entry.message === message,
+  );
+  sentMessages.push({ target, message });
+  return result;
+}
+
 export function slackLink(link: string, text: string): string {
   return `<${link}|${text}>`;
 }
