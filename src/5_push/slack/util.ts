2a27e9b8af9e8adbadb4b8ad442229e44dfa5d65
Amy Manny <amyryanmanny@github.com>
2025-10-24 14:56:04 -0700

    Style

--- src/5_push/slack/util.ts
+++ src/5_push/slack/util.ts
@@ -1,10 +1,16 @@
-const sentMessages = Array<{ target: string; message: string }>();
+type SentMessage = {
+  target: string;
+  message: string;
+};
+
+const sentMessages: SentMessage[] = [];
+
 export function isDuplicate(target: string, message: string): boolean {
   // Prevent sending duplicate messages in the same run
   const result = sentMessages.some(
     (entry) => entry.target === target && entry.message === message,
   );
-  sentMessages.push({ target, message });
+  sentMessages.push({ target, message }); // Remember for next time
   return result;
 }
 
