9cd7207fe46c8ce7c0cef7e460ac2b8fb51fdde6
Amy Manny <amyryanmanny@github.com>
2025-10-24 15:10:08 -0700

    Always log to console even if sending notices on GHA

--- src/util/log.ts
+++ src/util/log.ts
@@ -13,17 +13,15 @@ export function emitInfo(message: string) {
 export function emitWarning(message: string) {
   if (isGitHubAction()) {
     warning(message);
-  } else {
-    console.warn(message);
   }
+  console.warn(message);
 }
 
 export function emitError(message: string) {
   if (isGitHubAction()) {
     error(message);
-  } else {
-    console.error(message);
   }
+  console.error(message);
 }
 
 // Summary
